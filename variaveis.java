8bc197ce06234afd01dcec3ece71087b9427b42c
myanners <118886212+myanners@users.noreply.github.com>
2023-03-13 00:15:05 -0300

    Tipos primitivos

---
+++ variaveis.java
@@ -0,0 +1,30 @@
+package aula1Variaveis;
+
+public class Variaveis {
+	//int - usado para armazenar valores inteiros (32 bits - de -2^31 a 2^31-1)
+	int minhaVariavel = 10;
+	int idade = 27;
+	int populacao = 1000000;
+	//double - usado para armazenar valores de ponto flutuante (64 bits)
+	double preco = 9.99;
+	double pi = 3.14159265359;
+	//boolean - usado para armazenar valores booleanos (verdadeiro ou falso)
+	boolean estaChovendo = true;
+	boolean ehMaiorDeIdade = false;
+	//char - usado para armazenar caracteres (16 bits que representa um único caractere Unicode)
+	char letra = 'a';
+	char simbolo = '$';
+	//byte - usado para armazenar valores inteiros pequenos (8 bits armazena números de -128 a 127)
+	byte idade2 = 27;
+	byte temperatura = -10;
+	//short - usado para armazenar valores inteiros curtos (16 bits - números inteiros na faixa de -32,768 a 32,767)
+	short distancia1 = 5000;
+	short populacao2 = 30000;
+	//long - usado para armazenar valores inteiros longos ( 64 bits - de -2^63 a 2^63-1)
+	long populacao3 = 1000000000;
+	long distancia2 = 2000000000;
+	//float - usado para armazenar valores de ponto flutuante de precisão simples (32 bits e é é indicado pela letra "f" no final do valor)
+	float preco2 = 9.99f;
+	float altura = 1.85f;
+
+}
